c4ef4501ad78649dfc20171c6176a2300719c834
Wendy <wendytsai1988@gmail.com>
2019-02-13 10:05:17 +0100

    Add 4th bonus feature and a .yml file for extracted messages

--- lesson_2/calculator_bonus_1.2.rb
+++ lesson_2/calculator_bonus_1.2.rb
@@ -1,4 +1,8 @@
-# improvement 3: Amend operation_to_message method
+# improvement 4: extract messages into a ymal file, except message containing string interpolation
+# Remove the HEREDOC of operator_prompt, and replace with strings in ymal file.
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+# puts MESSAGES.inspect
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -29,13 +33,13 @@ def operation_to_message(op)
   message
 end
 
-prompt("Welcome! Enter your name:")
+prompt(MESSAGES['welcome'])
 
 name = ''
 loop do
   name = Kernel.gets().chomp()
   if name.empty?()
-    prompt("Make sure to use a valid name.")
+    prompt(MESSAGES['valid_name'])
   else
     break
   end
@@ -47,38 +51,29 @@ loop do # main loop
 
   number1 = ''
   loop do
-    prompt("What's the first number?")
+    prompt(MESSAGES['first_number'])
     number1 = Kernel.gets().chomp()
 
     if valid_number(number1)
       break
     else
-      prompt("This doesn't look like a valid number")
+      prompt(MESSAGES['warning'])
     end
   end
 
   number2 = ''
   loop do
-    prompt("What's the second number?")
+    prompt(MESSAGES['second_number'])
     number2 = Kernel.gets().chomp()
     if valid_number(number2)
       break
     else
-      prompt("This doesn't look like a valid number")
+      prompt(MESSAGES['warning'])
     end
   end
 
-  
-  operator_prompt = <<-question
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
 
-  question
-
-  prompt(operator_prompt)
+  prompt(MESSAGES['operator_prompt'])
 
   operator = ''
   loop do
@@ -86,7 +81,7 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Must choose 1,2,3 or 4")
+      prompt(MESSAGES['valid_choice'])
     end
   end
 
@@ -101,7 +96,7 @@ loop do # main loop
             number1.to_f() * number2.to_f()
            when '4' 
             while !(valid_number(number2) && (number2!= "0"))
-              prompt("Sorry, divider can not be 0. Please give another number:")
+              prompt(MESSAGES['divider_0_warning'])
               number2 = Kernel.gets().chomp()
             end 
             number1.to_f() / number2.to_f()
@@ -113,10 +108,10 @@ loop do # main loop
 
   prompt("The result is #{result}")
 
-  prompt("Do you want to perfom another calculation? (Type y to calculate again)")
+  prompt(MESSAGES['play_again'])
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 
 end # main loop finish
 
-prompt("Thank you & Good bye!")
+prompt(MESSAGES['goodbye'])
